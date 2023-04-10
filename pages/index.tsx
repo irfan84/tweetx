00148013c4b810b3435b34129c424d45d89aa154
Irfan Shakeel <mirfanshakeel84@gmail.com>
2023-04-10 21:34:54 +0500

    Added Posts and Comments Functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
 import Header from '../components/Header'
+import Form from '../components/Form'
+import PostFeed from '../components/posts/PostFeed'
+
 const Home: NextPage = () => {
   return (
     <>
       <Header label='Home' />
+      <Form placeholder="What's Happening" />
+      <PostFeed />
     </>
   )
 }
